7eb849f25cd3460dc323487e69b3fca62adff58b
Ryan Williams <ryancwilliams9@gmail.com>
2012-03-26 23:51:51 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Punkshows::Application.config.secret_token = '075e3bfe1c046c97b8264ca5bd864ce044ddb858a13be9ed92510343a6953479ceb20a508a2c08c0212e86f485483aaade1b0bd6d1417e2bee4a15ef470dfd30'
