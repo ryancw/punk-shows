d917b7bc84db5920b5ad4537e69ce098d27099fa
sicentendu <ryancwilliams9@gmail.com>
2012-10-01 14:49:32 -0400

    more routing with shows/microposts, will probably break something

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Punkshows::Application.routes.draw do
   resources :sessions,      only: [:new, :create, :destroy]
   resources :microposts,    only: [:create, :destroy, :show, :edit, :update]
   resources :relationships, only: [:create, :destroy]
-  resources :microposts, :path => 'shows'
-
+  resources :microposts, :path => 'show'
+  resources :shows, :controller => "microposts", :as => "microposts"
 
   root to: 'static_pages#home'
 
